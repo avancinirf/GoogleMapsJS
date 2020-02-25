e92d60ebd7e7ba35ff3ae7d4d96ad86f54912721
avancinirf <avancini.rf@gmail.com>
2020-02-25 18:16:09 +0000

    GMJS - Inclusao de google layers e geojson

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,70 @@
+
+SATELITE = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+  maxZoom: 20,
+  subdomains:['mt0','mt1','mt2','mt3'],
+  attribution: '<a href="http://www.consigsa.com"> Desenvolvido </b>por Consig</a>'
+});
+
+TERRAIN = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+  maxZoom: 20,
+  subdomains:['mt0','mt1','mt2','mt3'],
+  attribution: '<a href="http://www.consigsa.com"> Desenvolvido </b>por Consig</a>'
+});
+
+//var map = L.map('map').setView([51.505, -0.09], 13);
+
+//SATELITE.addTo(map);
+
+
+//////////
+
 var map = L.map('map').setView([-22.9532, -43.27315], 13);
-//Centraliza todo layer escolhido (municipiosLayer) na tela, ignora o zoom da variável map
-//map.fitBounds(municipiosLayer.getBounds());
+//Centraliza layer na tela, ignora o zoom da variável map
+//map.fitBounds(layer.getBounds());
 
 
-// Adicionando imagem de satélite
-// Adição da camada do Google Maps
-var googleLayer = new L.Google();
-var googleRoadmapLayer = new L.Google('ROADMAP');
-var googleTerrainLayer = new L.Google('TERRAIN');
-map.addLayer(googleLayer);
+// Adicionando baseLayer default no mapa
+map.addLayer(SATELITE);
+
+
+
+
+var fotoIcon = L.icon({
+
+  iconUrl: './src/img/camera-icon-2235488.png',
+  iconSize:     [30, 20],
+  iconAnchor:   [12, 20],
+  popupAnchor:  [-3, -15]
+});
+
+// add markers with popup info
+function onEachFeature_pontos(feature, layer) {
+  // TODO 
+}
+
+
+var geojsonFloresta = L.geoJson(floresta).addTo(map);
+var geoJsonPontos = L.geoJson(pontos, {
+  // Função de popup com dados do geoJson
+  onEachFeature: onEachFeature_pontos,
+  // Altera o ícone de um geoJson
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {icon: fotoIcon});
+  }
+}).bindPopup(function (layer) {
+  return layer.feature.properties.nome;
+  }).addTo(map);
 
 // Adicionando as variáveis aos controles no mapa
 var baseMaps = {
-  'Google' : googleLayer,
-  'Google Roadmap' : googleRoadmapLayer,
-  'Google Terrain' : googleTerrainLayer
+  'Satélite' : SATELITE,
+  'Terreno' : TERRAIN,
 };
-//var overlayMaps = {
-//  'Floresta da Tijuca' : geojson,
-//  'Pontos turísticos'  : geoJsonLayer_pontos
-//};
+var overlayMaps = {
+  'Floresta da Tijuca' : geojsonFloresta,
+  'Pontos turísticos'  : geoJsonPontos
+};
 
 
 // Adicionando os Layers no controle de camadas
-//L.control.layers(baseMaps, overlayMaps).addTo(map);
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlayMaps).addTo(map);
